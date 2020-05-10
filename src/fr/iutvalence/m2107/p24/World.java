4850452dcb2e5d170f5f870b31fcb6f7bc97b10e
Peyronn <peyronnn@example.com>
2020-05-10 21:53:15 +0200

    Create World class and add unimplemented methods

---
+++ src/fr/iutvalence/m2107/p24/World.java
@@ -0,0 +1,53 @@
+package fr.iutvalence.m2107.p24;
+
+import java.awt.Graphics;
+
+public class World extends GameState
+{
+	
+	public Room[] rooms;
+	public Mob[] mobs;
+	public Player player;
+
+	public World(GameStateManager gsm)
+	{
+		super(gsm);
+		
+	}
+
+	@Override
+	public void init()
+	{
+		 
+		
+	}
+
+	@Override
+	public void tick()
+	{
+		 
+		
+	}
+
+	@Override
+	public void draw(Graphics p0)
+	{
+		 
+		
+	}
+
+	@Override
+	public void keyPressed(int p0)
+	{
+		 
+		
+	}
+
+	@Override
+	public void keyReleased(int p0)
+	{
+		 
+		
+	}
+
+}
