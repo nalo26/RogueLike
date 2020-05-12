3439a6a92f24cca3a078656a6e49be434661b141
maxte <maxte@DESKTOP-DA11N4K>
2020-05-12 23:01:31 +0200

    Javadoc for Room and Position.

--- src/fr/iutvalence/m2107/p24/Position.java
+++ src/fr/iutvalence/m2107/p24/Position.java
@@ -2,18 +2,36 @@ package fr.iutvalence.m2107.p24;
 
 public class Position {
 
+	/**
+	 * The x position in the map
+	 */
 	private final int x;
+	
+	/**
+	 * The y position in the map
+	 */
 	private final int y;	
 	
+	/**
+	 * Constructor of Position, set X and Y position.
+	 * @param theX position in the map
+	 * @param theY position in the map
+	 */
 	public Position(int theX, int theY) {
 		this.x = theX;
 		this.y = theY;
 	}
 	
+	/**
+	 * @return X position (Getter)
+	 */
 	public int getX() {
 		return  this.x;
 	}
 	
+	/**
+	 * @return Y position (Getter)
+	 */
 	public int getY() {
 		return this.y;
 	}
--- src/fr/iutvalence/m2107/p24/Room.java
+++ src/fr/iutvalence/m2107/p24/Room.java
@@ -5,19 +5,41 @@ import java.util.List;
 
 public class Room {
 	
+	/**
+	 * Default size of the room.
+	 */
 	public static final int DEFAULT_SIZE = 20;
 	
+	/**
+	 * List of all mobs in this room.
+	 */
 	private List<Mob> mobs = new ArrayList<Mob>();
+	
+	/**
+	 * The Board of the game.
+	 * we make a two-dimensional table because we've got a height and a width so 2 dimension.
+	 * TTT board upper-left corner is at [0][0], TTT board lower-left corner is at [DEFAULT_SIZE][DEFAULT_SIZE].
+	 */
 	private int[][] roomBoard = new int[DEFAULT_SIZE][DEFAULT_SIZE];
 	
+	/**
+	 * Constructor of your Room.
+	 */
 	public Room() {
 		
 	}
 
+	/**
+	 * @return the list of mobs (Getter)
+	 */
 	public List<Mob> getMobs() {
 		return this.mobs;
 	}
 	
+	/**
+	 * 
+	 * @return the board of your game (Getter)
+	 */
 	public int[][] getBoard() {
 		return this.roomBoard;
 	}
