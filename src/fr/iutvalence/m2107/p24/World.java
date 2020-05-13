42fc3a0cbd8eb2f85e02f6c398308ab7bea40335
Peyronn <peyronnn@example.com>
2020-05-13 14:15:44 +0200

    rooms array is now a bidimensional array

--- src/fr/iutvalence/m2107/p24/World.java
+++ src/fr/iutvalence/m2107/p24/World.java
@@ -4,75 +4,75 @@ import java.awt.Graphics;
 
 public class World extends GameState
 {
-	
+
 	/**
-	 * Rooms of the World.
-	 * We make a one-dimensional table because the world have many Rooms.
+	 * Rooms of the World. We make a one-dimensional table because the world have
+	 * many Rooms.
 	 */
-	public Room[] rooms;
-	
+	public Room[][] rooms;
+
 	/**
-	 * Mobs of the World.
-	 * We make a one-dimensional table because the world have many Mobs.
+	 * Mobs of the World. We make a one-dimensional table because the world have
+	 * many Mobs.
 	 */
 	public Mob[] mobs;
-	
+
 	/**
 	 * The Player in the World.
 	 */
 	public Player player;
 
 	/**
-	 * Constructor of the World. 
+	 * Constructor of the World.
+	 * 
 	 * @param gsm State of the game.
 	 */
 	public World(GameStateManager gsm)
 	{
 		super(gsm);
-		
+
 	}
 
 	@Override
 	/**
-	 * Initialization of your game.
-	 * We set the player and then we create all the rooms.
+	 * Initialization of your game. We set the player and then we create all the
+	 * rooms.
 	 */
 	public void init()
 	{
-		 this.player = new Player();
-		 for(int i = 0; i < this.rooms.length; i++)
-		 {
-			 this.rooms[i] = new Room();
-		 }
-		
+		this.player = new Player();
+		for (int i = 0; i < this.rooms.length; i++)
+		{
+			for (int j = 0; j < this.rooms.length; j++)
+			{
+				this.rooms[i][j] = new Room();
+			}
+		}
+
 	}
 
 	@Override
 	public void tick()
 	{
-		 
-		
+
 	}
 
 	@Override
 	public void draw(Graphics p0)
 	{
-		 
-		
+
 	}
 
 	@Override
 	public void keyPressed(int p0)
 	{
-		 
-		
+
 	}
 
 	@Override
 	public void keyReleased(int p0)
 	{
-		 
-		
+
 	}
 
 }
