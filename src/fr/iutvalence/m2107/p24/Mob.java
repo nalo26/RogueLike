c2024031cfe2668975cc08b69796ea0f7fa06ca3
nalo_ <nath.v26@gmail.com>
2020-05-23 11:36:19 +0200

    Added life indicator.

--- src/fr/iutvalence/m2107/p24/Mob.java
+++ src/fr/iutvalence/m2107/p24/Mob.java
@@ -1,7 +1,9 @@
 package fr.iutvalence.m2107.p24;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import fr.iutvalence.m2107.p24.ressources.Images;
 
@@ -59,7 +61,17 @@ public class Mob {
 
 	public void draw(Graphics g) {
 		g.drawImage(this.image, this.position.getX(), this.position.getY(), null);
+		g.setColor(Color.BLACK);
 		g.drawRect(this.position.getX(), this.position.getY(), this.image.getWidth(), this.image.getHeight());
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(this.position.getX()+this.image.getWidth()/2-52/2, this.position.getY()-20, 52, 10);
+		g.setColor(Color.BLACK);
+		g.fillRect(this.position.getX()+this.image.getWidth()/2-52/2+1, this.position.getY()-19, 50, 8);
+		int red = (int)(255 * (1 - this.health / DEFAULT_HEALTH));
+		int green = (int)(255 * (this.health / DEFAULT_HEALTH));
+		g.setColor(new Color(red, green, 0));
+		g.fillRect(this.position.getX()+this.image.getWidth()/2-52/2+1, this.position.getY()-19, (int)(50 * (this.health / DEFAULT_HEALTH)), 8);
 	}
 	
 	/**
--- src/fr/iutvalence/m2107/p24/Player.java
+++ src/fr/iutvalence/m2107/p24/Player.java
@@ -1,5 +1,6 @@
 package fr.iutvalence.m2107.p24;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
@@ -60,7 +61,17 @@ public class Player {
 	
 	public void draw(Graphics g) {
 		g.drawImage(this.image, this.position.getX(), this.position.getY(), null);
+		g.setColor(Color.BLACK);
 		g.drawRect(this.position.getX(), this.position.getY(), this.image.getWidth(), this.image.getHeight());
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(this.position.getX()+this.image.getWidth()/2-52/2, this.position.getY()-20, 52, 10);
+		g.setColor(Color.BLACK);
+		g.fillRect(this.position.getX()+this.image.getWidth()/2-52/2+1, this.position.getY()-19, 50, 8);
+		int red = (int)(255 * (1 - this.health / DEFAULT_HEALTH));
+		int green = (int)(255 * (this.health / DEFAULT_HEALTH));
+		g.setColor(new Color(red, green, 0));
+		g.fillRect(this.position.getX()+this.image.getWidth()/2-52/2+1, this.position.getY()-19, (int)(50 * (this.health / DEFAULT_HEALTH)), 8);
 	}
 	
 	public void keyPressed(int k) {
