ff0a4adab2b881c02425bc4eff764a73dc551e95
peyronnn <peyronnn@example.com>
2020-05-25 16:00:49 +0200

    Create inventory class, fields, constructor and methods

---
+++ src/fr/iutvalence/m2107/p24/Inventory.java
@@ -0,0 +1,45 @@
+package fr.iutvalence.m2107.p24;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import fr.iutvalence.m2107.p24.items.Item;
+import fr.iutvalence.m2107.p24.ressources.Images;
+
+
+/**
+ * Represent the inventory of the player
+ *
+ */
+public class Inventory
+{
+	private ArrayList<Item> items;
+	
+	private BufferedImage inventoryImage;
+	
+	public Inventory()
+	{
+		this.items = null;
+		this.inventoryImage = Images.Inventory.getImage();
+	}
+	
+	public void addItem(Item i)
+	{
+		this.items.add(i);
+	}
+	
+	public void removeItem(Item i)
+	{
+		this.items.remove(i);
+	}
+	
+	public void draw(Graphics g)
+	{
+		g.drawImage(this.inventoryImage, GamePanel.WIDTH /2 - 625 , GamePanel.HEIGHT /2 + 100 ,250, 250, null);
+		if(this.items != null)
+		{
+			//TODO draw items here
+		}
+	}
+}
