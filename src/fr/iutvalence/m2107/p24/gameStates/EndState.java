ed8de62ed958936f2cb61dbcccd184d37fd98f4d
maxte <maxte@DESKTOP-DA11N4K>
2020-06-10 02:12:44 +0200

    Adding javadoc on EndState

--- src/fr/iutvalence/m2107/p24/gameStates/EndState.java
+++ src/fr/iutvalence/m2107/p24/gameStates/EndState.java
@@ -7,26 +7,34 @@ import java.awt.image.ImageObserver;
 
 import fr.iutvalence.m2107.p24.GamePanel;
 import fr.iutvalence.m2107.p24.ressources.Images;
-
+/**
+ * Represents the end menu when we finish the game.
+ */
 public class EndState extends GameState implements ImageObserver{
-
+	/**
+	 * Initialize the possible options and the selector.
+	 * @param gsm the manager wanted.
+	 */
 	public EndState(GameStateManager gsm) {
 		super(gsm);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	/** {@inheritDoc} */
 	@Override
 	public boolean imageUpdate(Image arg0, int arg1, int arg2, int arg3, int arg4, int arg5) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
+	/** {@inheritDoc} */
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/** {@inheritDoc} */
 	@Override
 	public void draw(final Graphics g) {
 		g.setColor(new Color(0, 0, 0));
@@ -34,12 +42,14 @@ public class EndState extends GameState implements ImageObserver{
 		g.drawImage(Images.WIN.getImage(), 0, 0, GamePanel.WIDTH/2, GamePanel.HEIGHT/2, this);
 		
 	}
-
+	
+	/** {@inheritDoc} */
 	@Override
 	public void keyPressed(int k) {
 		
 	}
-
+	
+	/** {@inheritDoc} */
 	@Override
 	public void keyReleased(int p0) {
 		// TODO Auto-generated method stub
